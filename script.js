b0cb9f77326a18c9f9599ac35cdce8c9b83ef230
Aman Chandra <46893171+acdevs@users.noreply.github.com>
2022-10-02 08:01:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function setAngle(updateText){
         sys['sin(']='Math.sin((Math.PI/180)*';
         sys['cos(']='Math.cos((Math.PI/180)*';
         sys['tan(']='Math.tan((Math.PI/180)*';
-        sys['asin(']='(180/Math.PI)*Math.asin(';
-        sys['acos(']='(180/Math.PI)*Math.acos(';
-        sys['atan(']='(180/Math.PI)*Math.atan(';
+        sys['sin<sup>-1</sup>(']='(180/Math.PI)*Math.asin(';
+        sys['cos<sup>-1</sup>(']='(180/Math.PI)*Math.acos(';
+        sys['tan<sup>-1</sup>(']='(180/Math.PI)*Math.atan(';
     }else{
         displayText.innerHTML='RAD';
         updateText.innerHTML='deg';
@@ -18,9 +18,9 @@ function setAngle(updateText){
         sys['sin(']='Math.sin(';
         sys['cos(']='Math.cos(';
         sys['tan(']='Math.tan(';
-        sys['asin(']='Math.asin(';
-        sys['acos(']='Math.acos(';
-        sys['atan(']='Math.atan(';
+        sys['sin<sup>-1</sup>(']='Math.asin(';
+        sys['cos<sup>-1</sup>(']='Math.acos(';
+        sys['tan<sup>-1</sup>(']='Math.atan(';
     }
 }
 
@@ -28,7 +28,7 @@ let inverseState= false;
 function setInverse(updateText){
     let Elems= document.getElementsByClassName('inv');
     const revStates= ['sin', 'cos', 'tan', 'ln', 'log'];
-    const invStates= ['asin', 'acos', 'atan', 'exp', '10^'];
+    const invStates= ['sin<sup>-1</sup>', 'cos<sup>-1</sup>', 'tan<sup>-1</sup>', 'exp', '10^'];
     if (!inverseState){
         updateText.innerHTML='rev';
         inverseState= true;
@@ -46,30 +46,28 @@ function setInverse(updateText){
     }
 }
 
-let eval_express='';
-let expression=document.getElementById('expression');
-let mini_result=document.getElementById('mini_display');
+let eval_express=[];
+let expression_view=[];
+let expression= document.getElementById('display');
+let mini_result= document.getElementById('mini_display');
 
 const sys={
-    '9':'9','8':'8','7':'7','6':'6','5':'5','4':'4','3':'3','2':'2','1':'1','0':'0',
-    '.':'.','+':'+','-':'-','×':'*','÷':'/','%':'*0.01',
-    'sin(':'Math.sin(','cos(':'Math.cos(','tan(':'Math.tan(',
-    'ln(':'Math.log(','log(':'Math.log10(','√(':'Math.sqrt(',
-    'asin(':'Math.asin(','acos(':'Math.acos(','atan(':'Math.atan(',
-    'exp(':'Math.exp(','10^(':'Math.pow(10,',
-    '^':'**','π':'Math.PI','e':'Math.E',
-    '(':'(',')':')','!':'!'
+    '9':'9','8':'8','7':'7','6':'6','5':'5','4':'4','3':'3','2':'2','1':'1','0':'0','.':'.','+':'+','-':'-','×':'*','÷':'/','%':'*0.01',
+    'sin(':'Math.sin(','cos(':'Math.cos(','tan(':'Math.tan(','ln(':'Math.log(','log(':'Math.log10(','√(':'Math.sqrt(',
+    'sin<sup>-1</sup>(':'Math.asin(','cos<sup>-1</sup>(':'Math.acos(','tan<sup>-1</sup>(':'Math.atan(',
+    'exp(':'Math.exp(','10^(':'Math.pow(10,','^':'**','π':'Math.PI','e':'Math.E','(':'(',')':')','!':'!'
     };
 function express(putValue){
     let sym = putValue.getAttribute("value");
-    if (expression.value == 'Error'){
-        expression.value='';
-        eval_express='';
+    if (expression.innerTEXT == 'Error'){
+        expression.innerHTML= '';
+        eval_express=[];
     }
-    expression.value+=sym;
-    eval_express += sys[sym];
+    expression_view.push(sym);
+    expression.innerHTML= expression_view.join('');
+    eval_express.push(sys[sym]);
     expression.scrollBy(1000,0);
-    miniResult(eval_express);
+    miniResult(eval_express.join(''));
 }
 
 function miniResult(getIt){
@@ -88,20 +86,46 @@ function miniResult(getIt){
         result='. . .'
     }
     mini_result.innerHTML= result;
-    mini_result.scrollBy(1000,0);
     return result;
 }
 
 function backspace(){
-    expression.value='';
-    eval_express='';
-    mini_result.innerHTML='';
+    expression_view.pop();
+    expression.innerHTML= expression_view.join('');
+    eval_express.pop();
+    mini_result.innerHTML= miniResult(eval_express.join(''));
 }
+
 function display(){
-    let view=miniResult(eval_express);
+    let view= miniResult(eval_express.join(''));
     if (view == '. . .' || view == 'Not a number'){
         view='Error'
     }
-    expression.value=view;
+    expression_view= []
+    expression.innerHTML= view;
+    expression_view.push(view.toString());
+    eval_express= [];
+    eval_express.push(view.toString())
     mini_result.innerHTML='';
+}
+
+function clear(){
+    expression_view= [];
+    expression.innerHTML= expression_view.join('');
+    eval_express= [];
+    mini_result.innerHTML= miniResult(eval_express.join(''));
+}
+
+let toggle= false;
+function toggle_panel(){
+    let sci_pad= document.getElementById('sci_pad');
+    if( !toggle ){
+        sci_pad.classList.add('slideR');
+        sci_pad.classList.remove('slideL');
+        toggle= true;
+    }else{
+        sci_pad.classList.add('slideL');
+        sci_pad.classList.remove('slideR');
+        toggle= false;
+    }
 }
